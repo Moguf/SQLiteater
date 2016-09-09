576e208d29b7fe20f7f795a44b7512f5f5ec4173
Mogu programming <kbu94984@gmail.com>
2016-09-09 15:14:56 +0900

    FIX: commit in insert.

--- sqliteater/sqliteater.py
+++ sqliteater/sqliteater.py
@@ -150,7 +150,7 @@ class SQLiteater(object):
         instruction = 'insert into ' + tablename + self.list2p(_namelist) + \
                       ' values ' + self.list2p(datalist)
         self.crsr.execute(instruction)
-        self.crsr.commit()
+        self.conn.commit()
 
     def list2p(self, inlist):
         """ 
